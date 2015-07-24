af815603e50facc679f5d55ee911638b5b41d000
Breno Martinusso <martinusso@gmail.com>
2015-07-24 16:49:22 -0300

    Create a class function to AmazonEmailService and organized params as record

--- BuildQueryParameters.pas
+++ BuildQueryParameters.pas
@@ -3,20 +3,14 @@ unit BuildQueryParameters;
 interface
 
 uses
+  AmazonEmailMessage,
   AmazonEmailService,
   System.Classes;
 
 type
   TBuildQueryParameters = class
-  private
-    FEmailBody: TEmailBody;
   public
-    constructor Create(const EmailBody: TEmailBody);
-    function GetQueryParams(const Recipients: TArray<string>; const FromName, FromAddress, Subject, MessageBody: string):
-        TStringStream; overload;
-    function GetQueryParams(const Recipients, ReplyTo: TArray<string>; const FromName, FromAddress, Subject, MessageBody:
-        string): TStringStream; overload;
-    property EmailBody: TEmailBody read FEmailBody write FEmailBody;
+    class function GetQueryParams(const EmailMessage: TEmailMessage): TStringStream;
   end;
 
 implementation
@@ -26,21 +20,9 @@ uses
   EncodeQueryParams,
   System.NetEncoding;
 
-constructor TBuildQueryParameters.Create(const EmailBody: TEmailBody);
-begin
-  FEmailBody := EmailBody;
-end;
-
-function TBuildQueryParameters.GetQueryParams(const Recipients: TArray<string>; const FromName, FromAddress, Subject,
-    MessageBody: string): TStringStream;
-begin
-  Result := GetQueryParams(Recipients, nil, FromName, FromAddress, Subject, MessageBody);
-end;
-
-function TBuildQueryParameters.GetQueryParams(const Recipients, ReplyTo: TArray<string>; const FromName, FromAddress,
-    Subject, MessageBody: string): TStringStream;
+class function TBuildQueryParameters.GetQueryParams(const EmailMessage: TEmailMessage): TStringStream;
 const
-  ACTION = 'SendEmail';
+  Action = 'SendEmail';
 var
   I: Integer;
   BodyType, Source: string;
@@ -48,24 +30,25 @@ begin
   Result := TStringStream.Create(EmptyStr, TEncoding.UTF8);
   try
     Result.WriteString('Action=' + ACTION);
-    Source := Format('=?utf-8?B?%s?= <%s>', [TNetEncoding.Base64.Encode(FromName), FromAddress]);
+
+    Source := Format('=?utf-8?B?%s?= <%s>', [TNetEncoding.Base64.Encode(EmailMessage.FromName), EmailMessage.FromAddress]);
     Result.WriteString(Format('&Source=%s', [TEncodeQueryParams.Encode(Source)]));
 
-    for I := Low(Recipients) to High(Recipients) do
-      Result.WriteString(Format('&Destination.ToAddresses.member.%d=%s', [I+1, TEncodeQueryParams.Encode(Recipients[I])]));
+    for I := Low(EmailMessage.Recipients) to High(EmailMessage.Recipients) do
+      Result.WriteString(Format('&Destination.ToAddresses.member.%d=%s', [I+1, TEncodeQueryParams.Encode(EmailMessage.Recipients[I])]));
 
-    for I := Low(ReplyTo) to High(ReplyTo) do
-      Result.WriteString(Format('&ReplyToAddresses.member.%d=%s', [I+1, TEncodeQueryParams.Encode(ReplyTo[I])]));
+    for I := Low(EmailMessage.ReplyTo) to High(EmailMessage.ReplyTo) do
+      Result.WriteString(Format('&ReplyToAddresses.member.%d=%s', [I+1, TEncodeQueryParams.Encode(EmailMessage.ReplyTo[I])]));
 
     Result.WriteString('&Message.Subject.Charset=UTF-8');
-    Result.WriteString(Format('&Message.Subject.Data=%s', [TEncodeQueryParams.Encode(Subject)]));
+    Result.WriteString(Format('&Message.Subject.Data=%s', [TEncodeQueryParams.Encode(EmailMessage.Subject)]));
 
-    if FEmailBody = eHTML then
+    if EmailMessage.BodyType = btHTML then
       BodyType := 'Html'
     else
       BodyType := 'Text';
     Result.WriteString(Format('&Message.Body.%s.Charset=UTF-8', [BodyType]));
-    Result.WriteString(Format('&Message.Body.%s.Data=%s', [BodyType, TEncodeQueryParams.Encode(MessageBody)]));
+    Result.WriteString(Format('&Message.Body.%s.Data=%s', [BodyType, TEncodeQueryParams.Encode(EmailMessage.Body)]));
   except
     Result.Free;
     raise
